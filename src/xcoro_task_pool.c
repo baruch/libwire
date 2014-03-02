a35b401ff566629aafc73ec28e5609d958345e26
Baruch Even <baruch@ev-en.org>
2014-03-02 23:46:12 +0200

    Allocate safe stacks for coroutine pools
    Allocate a stack space that is always a page size multiple and make sure
    that before and after it there is always a page that has no access
    permissions to create a SegFault if the process exceeds its stack space.
    This will prevent corruptions due to stack overflow.

--- src/xcoro_task_pool.c
+++ src/xcoro_task_pool.c
@@ -2,6 +2,8 @@
 #include "list.h"
 #include <stdlib.h>
 
+#include <sys/mman.h>
+
 struct wrapper_args {
 	void (*entry_point)(void *);
 	void *arg;
@@ -10,9 +12,27 @@ struct wrapper_args {
 	xcoro_task_pool_t *pool;
 };
 
+/* It should be possible to save address space if a large chunk of memory was
+ * mmaped and then between each two stacks there was only one guard page. In
+ * the current scheme there is a guard page before and after every stack.
+ */
 static void *stack_alloc(unsigned stack_size)
 {
-	return malloc(stack_size);
+	unsigned stack_size_pages = (stack_size + 4095) / 4096;
+	stack_size = stack_size_pages * 4096;
+	unsigned full_size = stack_size + 4096*2;
+	void *ptr = mmap(0, full_size, PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+	if (ptr == MAP_FAILED)
+		return NULL;
+
+	int ret = mprotect(ptr + 4096, stack_size, PROT_READ|PROT_WRITE);
+	if (ret < 0) {
+		perror("failed to mprotect stack for read/write");
+		munmap(ptr, full_size);
+		return NULL;
+	}
+
+	return ptr+4096;
 }
 
 int xcoro_task_pool_init(xcoro_task_pool_t *pool, xcoro_task_pool_entry_t *entries, unsigned size, unsigned stack_size)
