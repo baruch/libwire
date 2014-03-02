f07e8d3f6728ff99c1b1749064a32e9cff000cec
Baruch Even <baruch@ev-en.org>
2014-03-02 21:52:49 +0200

    Basic echo server

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 CFLAGS=-Iinclude -g -O0
+
+all: base echo_server
+
 base: test/base.o src/xcoro.o src/xcoro_fd.o
+	gcc -o $@ $^
+
+echo_server: test/echo_server.o src/xcoro.o src/xcoro_fd.o
 	gcc -o $@ $^
 
 .PHONY: clean
 clean:
-	rm -f test/base.o src/xcoro.o src/xcoro_fd.o base
+	rm -f test/base.o src/xcoro.o src/xcoro_fd.o base echo_server
---
+++ test/echo_server.c
@@ -0,0 +1,138 @@
+#include "xcoro.h"
+#include "xcoro_fd.h"
+#include "macros.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <memory.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+
+xcoro_t xcoro_main;
+
+xcoro_task_t task_accept;
+char task_accept_stack[4096];
+
+int echo_in_use;
+xcoro_task_t task_echo;
+char task_echo_stack[4096];
+
+void set_nonblock(int fd)
+{
+	int ret = fcntl(fd, F_GETFL);
+	if (ret < 0)
+		return;
+
+	fcntl(fd, F_SETFL, ret | O_NONBLOCK);
+}
+
+void set_reuse(int fd)
+{
+	int so_reuseaddr = 1;
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &so_reuseaddr, sizeof(so_reuseaddr));
+}
+
+int socket_setup(unsigned short port)
+{
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd < 1) {
+		perror("Failed to create socket");
+		return -1;
+	}
+
+	set_nonblock(fd);
+	set_reuse(fd);
+
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+
+	int ret = bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+	if (ret < 0) {
+		perror("Failed to bind to socket");
+		close(fd);
+		return -1;
+	}
+
+	ret = listen(fd, 100);
+	if (ret < 0) {
+		perror("failed to listen to port");
+		close(fd);
+		return -1;
+	}
+
+	return fd;
+}
+
+void task_echo_run(void *arg)
+{
+	int fd = (long int)arg;
+	int ret;
+
+	set_nonblock(fd);
+
+	do {
+		char buf[1024];
+		xcoro_fd_wait_read(fd);
+		ret = read(fd, buf, sizeof(buf));
+		if (ret == 0) {
+			break;
+		} else if (ret < 0) {
+			if (errno == EINTR || errno == EAGAIN)
+				continue;
+			else
+				break;
+		}
+
+		ret = write(fd, buf, ret);
+		if (ret < 0) {
+			if (errno == EINTR || errno == EAGAIN)
+				continue;
+			else
+				break;
+		}
+	} while (ret >= 0);
+
+	close(fd);
+	echo_in_use = 0;
+	printf("echo is done\n");
+}
+
+void task_accept_run(void *arg)
+{
+	int fd = socket_setup(9876);
+	if (fd < 0)
+		return;
+
+	while (1) {
+		xcoro_fd_wait_read(fd);
+		int new_fd = accept(fd, NULL, NULL);
+		if (new_fd >= 0) {
+			printf("New connection: %d\n", new_fd);
+			if (echo_in_use) {
+				printf("Echo is busy, sorry\n");
+				close(new_fd);
+			} else {
+				echo_in_use = 1;
+				xcoro_task_init(&task_echo, "echo", task_echo_run, (void*)(long int)new_fd, &task_echo_stack, sizeof(task_echo_stack));
+			}
+		} else {
+			if (errno != EINTR && errno != EAGAIN) {
+				perror("Error accepting from listening socket");
+				break;
+			}
+		}
+	}
+}
+
+int main()
+{
+	xcoro_init(&xcoro_main);
+	xcoro_fd_init();
+	xcoro_task_init(&task_accept, "accept", task_accept_run, NULL, &task_accept_stack, sizeof(task_accept_stack));
+	xcoro_run();
+	return 0;
+}
