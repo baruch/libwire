54b73ca9ecc0fa5aed754fcd97d8293dfec922ac
Baruch Even <baruch@ev-en.org>
2014-03-03 22:24:55 +0200

    Create an implicit pool for stacks
    This saves on wasted pages that are used for protection and reduces
    overhead of stack allocation to only mprotect on average.
    
    The unused stacks are only memory mapped but are not touched so the
    first use will require a page fault still.

--- src/xcoro_stack.c
+++ src/xcoro_stack.c
@@ -4,21 +4,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static void *base;
+static const unsigned alloc_size = 10*1024*1024;
+static unsigned used_size;
+
 void *xcoro_stack_alloc(unsigned stack_size)
 {
 	unsigned stack_size_pages = (stack_size + 4095) / 4096;
 	stack_size = stack_size_pages * 4096;
-	unsigned full_size = stack_size + 4096*2;
-	void *ptr = mmap(0, full_size, PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-	if (ptr == MAP_FAILED)
-		return NULL;
+	unsigned full_size = stack_size + 4096;
 
-	int ret = mprotect(ptr + 4096, stack_size, PROT_READ|PROT_WRITE);
+	if (base == NULL || used_size + full_size > alloc_size) {
+		void *ptr = mmap(0, alloc_size, PROT_NONE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+		if (ptr == MAP_FAILED) {
+			perror("Failed to allocate space for stacks");
+			return NULL;
+		}
+
+		base = ptr + 4096;
+		used_size = 4096;
+	}
+
+	void *ptr = base;
+	int ret = mprotect(ptr, stack_size, PROT_READ|PROT_WRITE);
 	if (ret < 0) {
 		perror("failed to mprotect stack for read/write");
-		munmap(ptr, full_size);
 		return NULL;
 	}
 
-	return ptr+4096;
+	base += stack_size + 4096;
+	used_size += stack_size + 4096;
+
+	return ptr;
 }
