a484386c0acb4cd04563861e322fc0234e31972e
Baruch Even <baruch@ev-en.org>
2014-03-04 00:17:03 +0200

    Start to document the library

--- include/xcoro.h
+++ include/xcoro.h
@@ -1,19 +1,74 @@
+/** @file
+ * XCoro main file. This includes the inner core of the XCoro library,
+ * everything else is only supporting this to make it easier to use and can be
+ * replaced if needed.
+ */
 #ifndef XCORO_LIB_H
 #define XCORO_LIB_H
 
-#define XCORO_TASK_SIZE sizeof(xcoro_task_t)
+/// @addtogroup Core
+/// @{
+
+/** The coroutine root.
+ *
+ * When xcoro will support multiple threads there will be one xcoro_t for each coroutine thread.
+ */
 typedef struct xcoro xcoro_t;
+
+/** The coroutine task. This represents an actual coroutine with all of its associated gear.
+ * It can be allocted by the user or brought it by the xcoro_task_pool.
+ */
 typedef struct xcoro_task xcoro_task_t;
 
+/** Initialize an XCoro coroutine thread.
+ *
+ * Give it an uninitialized but allocated space to work in and it will take care of it for this thread.
+ */
 void xcoro_init(xcoro_t *xcoro);
+
+/** Run the coroutine threads. This function only returns when there are no
+ * coroutines to run, you should have initialized one or more coroutines before
+ * calling it.
+ */
 void xcoro_run(void);
+
+/** Yield the CPU to the other coroutines. It will put the current running task
+ * to the end of the ready list and will let all the other tasks get a slice of
+ * the CPU too.
+ */
 void xcoro_yield(void);
+
+/** Get the current running task. Useful for when you need to give a task to some other XCoro function. */
 xcoro_task_t *xcoro_get_current_task(void);
+
+/** Checks if the current running task is the only one.
+ *
+ * This is mostly useful for a polling manager to see if it needs to sleep in the OS or to only check for new actions.
+ */
 int xcoro_is_only_task(void);
+
+/** Resumes a suspended task. Puts the task at the end of the ready list.
+ *
+ * A side effect of its action is to put a task at the end of the ready list even if it is already in the running list.
+ */
 void xcoro_resume(xcoro_task_t *task);
+
+/** Suspend the currently running task.
+ */
 void xcoro_suspend(void);
 
+/** Initialize a task and puts it on the ready list.
+ *
+ * You should give it an allocated task to initialize, the name of the task,
+ * the entry function and an argument as well as the stack and the stack size.
+ *
+ * The task will be put at the end of the ready list and will be run later on,
+ * the current task is not suspended or removed from the running list so if you
+ * are in a long busy loop you should release the CPU with xcoro_yield().
+ */
 xcoro_task_t *xcoro_task_init(xcoro_task_t *task, const char *name, void (*entry_point)(void *), void *task_data, void *stack, unsigned stack_size);
+
+/// @}
 
 #include "xcoro_private.h"
 
--- include/xcoro_fd.h
+++ include/xcoro_fd.h
@@ -1,9 +1,30 @@
 #ifndef XCORO_FD_LIB_H
 #define XCORO_FD_LIB_H
 
+/** @file
+ * XCoro file descriptor poll loop.
+ *
+ * This part of the library gives support for polling, sleeping and waiting
+ * until a file descriptor is active and requires attention. It assumes that
+ * the file descriptor is in a non-blocking state. It is expected to work for
+ * pipes and sockets only and not for files as reading and writing to a real
+ * file fd will result in a blocking action which should be avoided.
+ */
+
+/// @addtogroup FD Poller
+/// @{
+
+/** Initialize the XCoro file descriptor poller. This is currently global per app and should become local to an XCoro thread.
+ */
 void xcoro_fd_init(void);
 
+/** One shot waiting for an FD to become readable. This is the less performant
+ * cousing of the fd mode functions below. It will add and remove the fd from
+ * the polling mechanism for each call.
+ */
 int xcoro_fd_wait_read(int fd);
+
+/** Sleep in the poller for msecs time. The current coroutine is suspended and resumed back when the time passes. */
 int xcoro_fd_wait_msec(int msecs);
 
 typedef enum xcoro_fd_mode {
@@ -22,5 +43,7 @@ int xcoro_fd_mode_read(xcoro_fd_state_t *fd_state);
 int xcoro_fd_mode_write(xcoro_fd_state_t *fd_state);
 int xcoro_fd_mode_none(xcoro_fd_state_t *fd_state);
 void xcoro_fd_wait(xcoro_fd_state_t *fd_state);
+
+/// @}
 
 #endif
