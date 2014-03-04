2784f62ba8c67c370e5de45d691198059a7572ef
Baruch Even <baruch@ev-en.org>
2014-03-04 21:11:50 +0200

    Flesh out function params in task pool

--- include/xcoro_task_pool.h
+++ include/xcoro_task_pool.h
@@ -32,13 +32,28 @@ struct xcoro_task_pool {
 };
 typedef struct xcoro_task_pool xcoro_task_pool_t;
 
-/** Initialize a task pool. Sets the pool structure with the entries array of size size and will allocate stacks of size stack_size. */
+/** Initialize a task pool. Sets the pool structure with the entries array of size size and will allocate stacks of size stack_size.
+ *
+ * @param[in] pool The pool to initialize.
+ * @param[in] entries An array of all the task pool entries, this reserves the space for the tasks but they need not be initialized in advance or even zeroed. They can be left untouched. The stack pointer in them can either be preallocated or just left untouched and it will be allocate on the first use of the associated task, the stack will be allocated by using xcoro_stack_alloc() with the pool stack_size.
+ * @param[in] size The size of the entries array.
+ * @param[in] stack_size The stack size to be used for the tasks. If the stacks were preallocated they must all be the same size and the size needs to be reported correctly here.
+ * @return 0 for ok,<br>
+ *         -1 when an error occurred and the pool cannot be initialized.
+ */
 int xcoro_task_pool_init(xcoro_task_pool_t *pool, xcoro_task_pool_entry_t *entries, unsigned size, unsigned stack_size);
 
 /** Allocate a new task from the task pool. Gives the task a name and a
  * function to call and an argument. When the task finished it will
- * automatically be returned to the pool. If there is no entry available in the
- * pool it will return NULL. */
+ * automatically be returned to the pool.
+ *
+ * @param[in] pool The pool to allocate a task from.
+ * @param[in] name The name to give the task.
+ * @param[in] entry_point The entry point function, it takes a void pointer argument and returns nothing.
+ * @param[in] arg The argument to pass to the entry point function when it starts.
+ * @return An allocated and initialized task. If there is no entry available in the
+ * pool it will return NULL.
+ */
 xcoro_task_t *xcoro_task_pool_alloc(xcoro_task_pool_t *pool, const char *name, void (*entry_point)(void*), void *arg);
 
 /// @}
