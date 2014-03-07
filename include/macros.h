90e4916a3b8425f67fecaf02da92c0f5e951fd04
Baruch Even <baruch@ev-en.org>
2014-03-07 09:53:12 +0200

    Avoid redefinition of macros

--- include/macros.h
+++ include/macros.h
@@ -3,8 +3,12 @@
 
 #include <stddef.h>
 
+#ifndef ARRAY_SIZE
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#endif
 
+#ifndef container_of
 #define container_of(ptr, type, field) ((type*)((void*)ptr - (void*)offsetof(type, field)))
+#endif
 
 #endif
