14ea1466e3701e816984c7e11e4a6250a80dd454
Baruch Even <baruch@ev-en.org>
2014-07-11 22:29:47 +0300

    Slightly larger test of stdout logger

--- test/log_stdout.c
+++ test/log_stdout.c
@@ -7,12 +7,14 @@
 static wire_thread_t wire_main;
 static wire_t task_log1;
 static wire_t task_log2;
+static wire_t task_log3;
 
 static void do_log(void *arg)
 {
 	const char *name = arg;
 
 	wire_log(WLOG_INFO, "Start of wire %s (%p)", name, name);
+	wire_yield();
 	wire_log(WLOG_NOTICE, "End of wire %s", name);
 }
 
@@ -24,6 +26,7 @@ int main()
 	wire_log_init_stdout();
 	wire_init(&task_log1, "log 1", do_log, "log 1", WIRE_STACK_ALLOC(4*1024));
 	wire_init(&task_log2, "log 2", do_log, "log 2", WIRE_STACK_ALLOC(4*1024));
+	wire_init(&task_log3, "log 3", do_log, "log 3", WIRE_STACK_ALLOC(4*1024));
 	wire_thread_run();
 	return 0;
 }
