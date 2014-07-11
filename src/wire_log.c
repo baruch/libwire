35588a3844cb79f575158ad722126b9677fd0c9e
Baruch Even <baruch@ev-en.org>
2014-07-11 23:13:51 +0300

    Support multiple log lines pending in the buffer
    To reduce the chances of putting a wire to sleep have multiple log line
    buffers to write into.

--- src/wire_log.c
+++ src/wire_log.c
@@ -1,12 +1,14 @@
 #include "wire_log.h"
 #include "wire.h"
 #include "wire_io.h"
-#include "wire_lock.h"
+#include "wire_semaphore.h"
 
 #include <syslog.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <time.h>
+
+#define NUM_LINES 16
 
 struct log_line {
 	struct timespec t;
@@ -17,8 +19,10 @@ struct log_line {
 
 static wire_t stdout_wire;
 static char stdout_wire_stack[4096];
-static wire_lock_t lock;
-static struct log_line line;
+static wire_sem_t sem;
+static struct log_line lines[NUM_LINES];
+static int cur_log_line;
+static int cur_write_line;
 
 static const char *level_to_str(wire_log_level_e level)
 {
@@ -39,7 +43,8 @@ static void stdout_wire_func(void *arg)
 	UNUSED(arg);
 
 	while (1) {
-		if (line.str_len) {
+		struct log_line *line = &lines[cur_write_line];
+		if (line->str_len) {
 			time_t t;
 			struct tm tm;
 			char tm_str[24];
@@ -48,24 +53,27 @@ static void stdout_wire_func(void *arg)
 			size_t next_str_len;
 			struct iovec iov[3];
 
-			t = line.t.tv_sec;
+			t = line->t.tv_sec;
 			localtime_r(&t, &tm);
 			tm_str_len = strftime(tm_str, sizeof(tm_str), "%Y-%m-%d %H:%M:%S.", &tm);
 
-			next_str_len = snprintf(next_str, sizeof(next_str), "%09lu <%s> ", (long unsigned)line.t.tv_nsec, level_to_str(line.level));
+			next_str_len = snprintf(next_str, sizeof(next_str), "%09lu <%s> ", (long unsigned)line->t.tv_nsec, level_to_str(line->level));
 
 			iov[0].iov_base = tm_str;
 			iov[0].iov_len = tm_str_len;
 			iov[1].iov_base = next_str;
 			iov[1].iov_len = next_str_len;
-			iov[2].iov_base = line.str;
-			iov[2].iov_len = line.str_len;
+			iov[2].iov_base = line->str;
+			iov[2].iov_len = line->str_len;
 			wio_writev(1, iov, 3);
-			line.str_len = 0;
+			line->str_len = 0;
 
-			wire_lock_release(&lock);
+			wire_sem_release(&sem);
+			if (++cur_write_line == NUM_LINES)
+				cur_write_line = 0;
+		} else {
+			wire_suspend();
 		}
-		wire_suspend();
 	}
 }
 
@@ -77,25 +85,28 @@ static void wire_log_stdout(wire_log_level_e level, const char *fmt, ...)
 	clock_gettime(CLOCK_REALTIME, &t);
 
 	// Wait until logging is possible
-	wire_lock_take(&lock);
+	wire_sem_take(&sem);
+	struct log_line *line = &lines[cur_log_line];
+	if (++cur_log_line == NUM_LINES)
+		cur_log_line = 0;
 
-	line.t = t;
-	line.level = level;
+	line->t = t;
+	line->level = level;
 	va_start(ap, fmt);
-	line.str_len = vsnprintf(line.str, sizeof(line.str), fmt, ap);
+	line->str_len = vsnprintf(line->str, sizeof(line->str), fmt, ap);
 	va_end(ap);
 
-	if (line.str_len > sizeof(line.str)-1) {
-		line.str_len = sizeof(line.str)-1;
+	if (line->str_len > sizeof(line->str)-1) {
+		line->str_len = sizeof(line->str)-1;
 	}
-	line.str[line.str_len++] = '\n';
+	line->str[line->str_len++] = '\n';
 
 	wire_resume(&stdout_wire);
 }
 
 void wire_log_init_stdout(void)
 {
-	wire_lock_init(&lock);
+	wire_sem_init(&sem, NUM_LINES);
 	wire_init(&stdout_wire, "stdout logger", stdout_wire_func, NULL, stdout_wire_stack, sizeof(stdout_wire_stack));
 	wire_log = wire_log_stdout;
 }
