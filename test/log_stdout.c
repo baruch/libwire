bfb468e2804e4ae94539ca9bc8d1f5de7a54c53e
Baruch Even <baruch@ev-en.org>
2014-07-14 23:12:40 +0300

    Run log_stdout for wire_log coverage

--- test/log_stdout.c
+++ test/log_stdout.c
@@ -23,6 +23,7 @@ int main()
 	wire_thread_init(&wire_main);
 	wire_fd_init();
 	wire_io_init(1);
+	wire_log(WLOG_INFO, "Do not crash on logging before wire_log init");
 	wire_log_init_stdout();
 	wire_init(&task_log1, "log 1", do_log, "log 1", WIRE_STACK_ALLOC(4*1024));
 	wire_init(&task_log2, "log 2", do_log, "log 2", WIRE_STACK_ALLOC(4*1024));
